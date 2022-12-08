15335a6a1cce88b43d91d484880641017d6597dd
nguyen hai nguyen <nguyenhainguyen@bincg.com>
2022-12-08 14:26:15 +0700

    commit fundamentals páºart 2 challenge 4

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -2,28 +2,30 @@
 function calcAverage(score1, score2, score3) {
     return (score1 + score2 + score3) / 3;
 }
+
 function checkWinner() {
-    let avgDolphins = calcAverage(85,54,41)
-    let avgKoalas = calcAverage(23,34,27)
-    if(avgDolphins >= 2 * avgKoalas) {
+    let avgDolphins = calcAverage(85, 54, 41)
+    let avgKoalas = calcAverage(23, 34, 27)
+    if (avgDolphins >= 2 * avgKoalas) {
         console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`)
-    }
-    else if (avgKoalas >= 2 * avgDolphins) {
+    } else if (avgKoalas >= 2 * avgDolphins) {
         console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`)
-    }
-    else {
+    } else {
         console.log('Nothing')
     }
 }
+
 checkWinner();
 
 //Coding Challenge #2
-let bills = [123,555,44];
-function calcTip (value) {
+let bills = [123, 555, 44];
+
+function calcTip(value) {
     let tip = 50 <= value <= 300 ? value * 0.15 : value * 0.2;
     return `The bill was ${value}, the tip was ${tip}, and  the total value ${value + tip}`;
 }
-let tips = [calcTip(bills[0]),calcTip(bills[1]),calcTip(bills[2])];
+
+let tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 console.log(tips[0]);
 console.log(tips[1])
 console.log(tips[2])
@@ -31,16 +33,16 @@ console.log(tips[2])
 //Coding Challenge #3
 let John = {
     fullName: 'John Smith',
-    mass:78,
-    height:1.69,
+    mass: 78,
+    height: 1.69,
     calcBMI: function () {
         return this.mass / this.height ** 2
     }
 }
 let Mark = {
     fullName: 'Mark Miler',
-    mass:92,
-    height:1.95,
+    mass: 92,
+    height: 1.95,
     calcBMI: function () {
         return this.mass / this.height ** 2
     }
@@ -55,3 +57,28 @@ if (Mark.calcBMI() > John.calcBMI()) {
     );
 }
 
+//Coding Challenge #4
+
+function calcTip(bill) {
+    return 50 <= bill <= 300 ? bill * 0.15 : bill
+    0.2;
+}
+
+let bills2 = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+let tips2 = [];
+let totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+    const tip = calcTip(bills[i])
+    tips2.push(tip);
+    totals.push(tip + bills[i])
+}
+const calcAverage2 = (arr) => {
+    let sum = 0
+    for (var i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length
+
+}
+console.log(`Bill: ${calcAverage2(bills2)} Tip: ${calcAverage2(tips2)} Total: ${calcAverage2(totals)}`);
