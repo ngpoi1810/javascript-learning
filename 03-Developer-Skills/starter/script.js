487db02771765365b9baf1c2539df67af27bfbea
nguyen hai nguyen <nguyenhainguyen@bincg.com>
2022-12-13 16:59:23 +0700

    commit file challenge 1 03

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,13 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
+//Code challenge #1
+let arr1 = [1, 3, 2]
+function printForecast(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        let text = ''
+        text +=  String(a[i]);
+        console.log(text)
+    }
+}
+printForecast(arr1)
 
