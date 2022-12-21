20102b536878b6ecc249499f9c32122940740e78
nguyen hai nguyen <nguyenhainguyen@bincg.com>
2022-12-21 17:00:06 +0700

    commit challenge 5 6

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,49 @@
 'use strict';
+const checkButton = document.querySelector('.check');
+let againButton = document.querySelector('.again');
+let number = document.querySelector('.number').textContent;
+let numberRandom = Math.trunc(Math.random(number) * 20)
+let score = document.querySelector('.score').textContent
+let highScore = 0;
+console.log(numberRandom)
+function messageFunc(message) {
+    document.querySelector('.message').textContent = message
+}
+checkButton.addEventListener('click', function () {
+    let guess = Number(document.querySelector('.guess').value);
+    if (!guess) {
+        messageFunc('🚫 No Number')
+    } else if (guess === numberRandom) {
+        messageFunc('🎉 Correct Number')
+        document.querySelector('.number').textContent = numberRandom
+        document.body.style.background = 'rgb(96, 179, 71)';
+        document.querySelector('.number').style.width = '30rem'
+        if(Number(score) > highScore) {
+            highScore = score
+            document.querySelector('.highscore').textContent = highScore
+        }
+
+    }
+    else if (guess !== numberRandom) {
+        if(score > 1) {
+            messageFunc(guess > numberRandom ? '📈 Too high' : '📉 Too low')
+            score--;
+            document.querySelector('.score').textContent = score;
+        }
+        else {
+            document.querySelector('.score').textContent = '0';
+            messageFunc('😢 You loose')
+        }
+    }
+})
+
+againButton.addEventListener('click', function () {
+    document.body.style.background = '#222';
+    messageFunc('Start guessing...')
+    document.querySelector('.number').style.width = '15rem'
+    score = 20
+    numberRandom = Math.trunc(Math.random(number) * 20)
+    document.querySelector('.score').textContent = score
+    document.querySelector('.number').textContent = '?'
+    document.querySelector('.guess').value = ''
+})
--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const showModal = document.querySelectorAll('.show-modal');
+const closeModal = document.querySelector('.close-modal')
+for(let i = 0; i < showModal.length; i++) {
+    showModal[i].addEventListener('click', function (){
+        modal.classList.remove('hidden')
+        overlay.classList.remove('hidden')
+    })
+}
+function addClass() {
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+closeModal.addEventListener('click', function (){
+    addClass()
+})
+overlay.addEventListener('click',function (){
+    addClass()
+})
+document.addEventListener('keydown', function (e){
+    if(e.key === 'Escape' && !modal.classList.contains('.hidden')) {
+        addClass();
+    }
+})
