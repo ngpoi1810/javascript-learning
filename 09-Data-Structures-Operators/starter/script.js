75331116b8f7702e3a798476c67e2d63cd289ad8
nguyen hai nguyen <nguyenhainguyen@bincg.com>
2023-02-27 17:02:11 +0700

    commit

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -106,11 +106,57 @@ console.log(number)
 
 
 //Code challenge #1
-let players1 = [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'
-]
-let players2 = ['k','l','n','o','p','q','k','z','x','y']
-let bayernMunich = {
-    gk: 'Vinh', fieldPlayers: []
+const game = {
+    team1: 'Bayern Munich',
+    team2: 'Borrussia Dortmund',
+    players: [
+        [
+            'Neuer',
+            'Pavard',
+            'Martinez',
+            'Alaba',
+            'Davies',
+            'Kimmich',
+            'Goretzka',
+            'Coman',
+            'Muller',
+            'Gnarby',
+            'Lewandowski',
+        ],
+        [
+            'Burki',
+            'Schulz',
+            'Hummels',
+            'Akanji',
+            'Hakimi',
+            'Weigl',
+            'Witsel',
+            'Hazard',
+            'Brandt',
+            'Sancho',
+            'Gotze',
+        ]
+    ],
+    score: '4.0',
+    scored:['Lewandowski','Gnarby','Lewandowski','Hummels'],
+    date:'Nov 9th, 2037',
+    odds: {
+        team1:1.33,
+        x:3.25,
+        team2:6.5
+    }
 }
+const [players1, players2] = game.players
+console.log(players1)
+const [gk,...fieldPlayers] = players1
+console.log(gk,fieldPlayers)
+const allPlayers = [...players1,...players2]
+console.log(allPlayers)
+const {team1, x: draw, team2} = game.odds
+console.log(team1,draw,team2)
+const printGoals = function () {
+
+}
+
+
 
