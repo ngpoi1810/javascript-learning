e3ca9c18c0d0537c51923feb21b424282ca4f2c3
nguyen hai nguyen <nguyenhainguyen@bincg.com>
2023-03-24 16:58:26 +0700

    commit

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -30,3 +30,34 @@ createBooking('lh123',undefined,1000)
 * */
 
 //tìm hiểu về Bind, Apply, Call method----------------
+console.log('tìm hiểu về Bind, Apply, Call method----------------')
+const lufthansa = {
+    airline: 'Lufthansa',
+    iataCode: 'LH',
+    bookings: [],
+    book(flightNumber, name) {
+        console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNumber}`)
+        this.bookings.push({flight:`${this.iataCode}${flightNumber}`, name})
+    }
+
+}
+lufthansa.book(239, 'Hai Nguyen')
+lufthansa.book(142, 'Trung Vinh')
+console.log(lufthansa)
+const eurowings = {
+    airline: 'Eurowings',
+    iataCode: 'EW',
+    bookings:[]
+}
+//ta sẽ có một ví dụ sau
+//gán một biến = function book bên trong object lufthansa thì sẽ như thế nào
+const book = lufthansa.book;
+// book(23,'Hai Nguyen') //Sẽ ra lỗi vì đây là một regular function mà một
+//regular function thì keyword this sẽ là underfined
+//và làm sao để fix được và làm cách nào để xác định rõ this keyword sẽ trỏ vào đâu
+//đầu tiên ta sẽ dùng call method
+book.call(eurowings,23,'AAAA') // tham số đầu tiên là nơi this keyword trỏ tới còn lại
+//là 2 tham số của book method
+//Apply method cũng tương tự nhưng sau ghi truyền object để xác định this keyword trở thì những tham số sau phải cho vào một array ví dụ như sau
+const arrEuro = [222, 'BBBB']
+book.apply(eurowings,arrEuro)
