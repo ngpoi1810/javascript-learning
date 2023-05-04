0f1068ab6212c2f62fa2e010eea66f6eee65de90
nguyen hai nguyen <nguyenhainguyen@bincg.com>
2023-05-04 16:56:41 +0700

    commit 5/4/23

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,16 +10,49 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
-
-if (navigator.geolocation) {
+let map, mapEvent;
+if (navigator.geolocation)
     navigator.geolocation.getCurrentPosition(
         function (position) {
             const {latitude} = position.coords
             const {longitude} = position.coords
-            console.log(latitude, longitude)
+            const coords = [latitude, longitude]
+            map = L.map('map').setView(coords, 13);
+
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+
+            map.on('click', function (mapE) {
+                mapEvent = mapE
+                form.classList.remove('hidden')
+                inputDistance.focus()
+            });
         },
         function () {
-            alert('Could not get your position')
+            alert('Could not get your position');
         }
-    )
-}
+    );
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+    console.log(mapEvent)
+    const {lat, lng} = mapEvent.latlng
+    L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(
+            L.popup({
+                maxWidth: 250,
+                minWidth: 100,
+                autoClose: false,
+                closeOnClick: false,
+                className: 'running-popup',
+            })
+        )
+        .setPopupContent('Workout')
+        .openPopup();
+});
+
+
+
