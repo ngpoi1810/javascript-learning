039944168b43bfa8f55a5953e568796292a98ff1
nguyen hai nguyen <nguyenhainguyen@bincg.com>
2023-06-08 16:53:47 +0700

    commit

--- 18-forkify/starter/src/js/model.js
+++ 18-forkify/starter/src/js/model.js
@@ -1,29 +1,25 @@
-import {API_URL} from "./config";
+import { API_URL } from './config';
+import { getJSON } from './views/helpers';
 
 export const state = {
-    recipe: {},
-}
-export const loadRecipe = async function(id) {
-    try {
-        const res = await fetch(`${API_URL}/${id}`)
-        const data = await res.json();
-
-        if (!res.ok) throw new Error(`${data.message} (${res.status})`)
-
-        const {recipe} = data.data
-        state.recipe = {
-            id: recipe.id,
-            title: recipe.title,
-            publisher: recipe.publisher,
-            sourceURL: recipe.source_url,
-            imgage: recipe.image_url,
-            servings: recipe.servings,
-            cookingTime: recipe.cooking_time,
-            ingredients: recipe.ingredients
-        }
-        console.log(recipe)
-    } catch (err) {
-        alert(err)
-    }
-
-}
+  recipe: {},
+};
+export const loadRecipe = async function (id) {
+  try {
+    const data = await getJSON(`${API_URL}/${id}`)
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceURL: recipe.source_url,
+      imgage: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+  } catch (err) {
+    console.log(`${err}`);
+  }
+};
