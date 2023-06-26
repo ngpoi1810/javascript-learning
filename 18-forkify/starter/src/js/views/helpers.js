8a55e5a2ead4c72b432e8a5093b7169c50922154
nguyen hai nguyen <nguyenhainguyen@bincg.com>
2023-06-26 16:57:47 +0700

    'fix'

--- 18-forkify/starter/src/js/views/helpers.js
+++ 18-forkify/starter/src/js/views/helpers.js
@@ -7,13 +7,6 @@ const timeout = function (s) {
 };
 
 export const getJSON = async function(url) {
-<<<<<<< HEAD
-    try {
-        const res = await fetch(url)
-    } catch (err){
-        throw err;
-    }
-=======
    try{
     const res = await fetch(url)
     const data = await res.json();
@@ -23,5 +16,4 @@ export const getJSON = async function(url) {
    }catch(err) {
     console.log(err);
    }
->>>>>>> 24f5ea31b1432fa9513e6c393e0e89df6cc622b4
 }
